8d0d2b519de4adfad4ef9e7b69d86cbecc18ad07
Houizy <1204329383@qq.com>
2024-10-11 14:43:06 +0800

    solve modules1,2,3

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
@@ -11,9 +11,10 @@ mod sausage_factory {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
-        get_secret_recipe();
+    pub fn make_sausage() {
+        let _secret_recipe=get_secret_recipe();
         println!("sausage!");
+        println!("Using the secret recipe:{}",_secret_recipe);
     }
 }
 
